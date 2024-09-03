c8f76328ca0f1742d20c5dc43020a8166d1ebf21
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-09-03 18:21:45 +0530

    c3

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Welcome to PSR Vlog</h1>
+      <button className='users-button'>Users</button>
       </header>
     </div>
   );
