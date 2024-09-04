2d8367edec48f9071462941602e26d560ae51a35
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-09-04 16:15:22 +0530

    c10

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Users from './Users';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
+      <Route path='/Users' element={<Users/>}/>
     </Routes>
   </BrowserRouter>
 );
