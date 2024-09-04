b85a002bb2140c5e431a61d6124e7709a426a2ea
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-09-04 12:23:19 +0530

    c9

--- src/UserForm.js
+++ src/UserForm.js
@@ -7,9 +7,28 @@ const UserForm = props => {
                     backgroundColor: '#ffffff',
                     marginBottom: '30px',
                     display: 'block',
-
                 }}
             >
+                <Grid item xs={12}>
+                    <Typography component={'h1'} sx={{color: '#000000'}}>User Form</Typography>
+                </Grid>
+                <Grid>
+                    <Typography 
+                        component={'label'} 
+                        htmlFOr='id'
+                        sx={{
+
+                        }}
+                        >
+                            ID
+                        </Typography>
+                        <input
+                            type="number"
+                            id="id"
+                            name="id"
+                            sx={{width: '400px'}}
+                        />
+                </Grid>
 
             </Grid>
     );
