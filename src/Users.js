87d2466db1e922025d61ddb7570d13820cb4d63f
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-09-04 21:52:16 +0530

    c9

--- src/Users.js
+++ src/Users.js
@@ -1,5 +1,16 @@
 import UserForm from "./UserForm";
 
+const users = [
+    {
+        id: 1,
+        name: 'Prasad',
+    },
+    {
+        id: 2,
+        name: 'Prasadi',
+    }
+]
+
 const Users = () => {
     return (
         <UserForm/>
