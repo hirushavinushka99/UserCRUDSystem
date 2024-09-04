c24cde1365b13784942cfc53d0d2a03ac331fc2b
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-09-04 21:27:29 +0530

    c8

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { useNavigate } from "react-router-dom";
 import './App.css';
 
 function App() {
+  
+  const navigate = useNavigate();
+
   return (
     <div className="App">
       <header className="App-header">
       <h1>Welcome to PSR Vlog</h1>
-      <button className='users-button'>Users</button>
+      <button className='users-button' onClick={() => navigate('/users')}>Users</button>
       </header>
     </div>
   );
