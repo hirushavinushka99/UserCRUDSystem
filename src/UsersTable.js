908582c328c9580d8396ba40fde98256044f252a
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-09-05 11:43:49 +0530

    c3

--- src/UsersTable.js
+++ src/UsersTable.js
@@ -1,4 +1,4 @@
-import { Paper, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Table, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 
 const UsersTable = ({rows}) => {
     <TableContainer component={Paper}>
@@ -12,11 +12,27 @@ const UsersTable = ({rows}) => {
             </TableHead>
             <TableBody>
                 {
-                rows.map(row => (
+                rows.length > 0 ? rows.map(row => (
                     <TableRow key={row.id} sx={{'&:last-child td, &:last-child th' :{border:0}}}>
                         <TableCell component='th' scope='row'>{row.id}</TableCell>
+                        <TableCell component='th' scope='row'>{row.name}</TableCell>
+                        <TableCell>
+                            <Button
+                                sx={{margin:'0px 10px'}}
+                                onClick={() => ()}>Update</Button>
+
+                            <Button
+                                sx={{margin:'0px 10px'}}
+                                onClick={() => ()}>Delete</Button>
+
+                        </TableCell>
                     </TableRow>
-                    ))
+                    )) : (
+                        <TableRow sx={{'&:last-child td, &:last-child th' :{border:0}}}>
+                            <TableCell component='th' scope='row'>No Data</TableCell>
+                        </TableRow>
+                        
+                    )
                 }
             </TableBody>
         </Table>
