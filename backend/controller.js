462f49b55aca3190e14813aef152e1fc1a995fcf
Hirusha De Silva <169069562+hirushavinushka99@users.noreply.github.com>
2024-09-05 19:05:27 +0530

    c7

--- backend/controller.js
+++
@@ -1,3 +0,0 @@
-const app = require('./app');
-
-const server = app.listen(3001, '127.0.0.1');
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+const server = app.listen(3001, '127.0.0.1', () => {
+    console.log(`Node server is listening to ${server.address().part}`);
+    
+}); 
